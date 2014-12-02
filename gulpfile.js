7cc64314d401c773606fc8fa68509bdf6ff3b41f
Raheman Vaiya <rvaiya@qplx.org>
2014-12-01 20:04:17 -0500

    partial

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
+var fs = require('fs');
 var gulp = require('gulp');
 var rg = require('rangle-gulp');
+var inject = require('gulp-inject');
 var colors = require('colors');
 var runSequence = require('run-sequence');
+var angularFileSort = require('gulp-angular-filesort');
+var watch = require('gulp-watch');
+var bower = require('gulp-bower');
+var sass = require('gulp-sass');
+var del = require('del');
+var cp = require('ncp').ncp;
+var through = require('through2');
 
-var appFiles =  ['src/app/app.js','src/app/**/*.js', '!./src/app/**/*.test.js'];
-var testFiles =  ['src/app/app.js','src/app/**/*.js'];
+
+
+var htmlFiles = ['src/app/**/*html', 'src/index.html'];
+var angularFiles = ['!src/app/**/*.test.js',
+  'src/app/app.js',
+  'src/app/services/*.js',
+  'src/app/**/*-directive.js',
+  'src/app/**/*-controller.js'
+];
+
+var appFiles = ['src/app/app.js', 'src/app/**/*.js', '!./src/app/**/*.test.js'];
+var testFiles = ['./src/app/**/*.test.js'];
 var karmaFile = './testing/karma.conf.js';
+
 var karamConfig = {
-  karmaConf : karmaFile,
-  files : testFiles,
+  karmaConf: karmaFile,
+  files: testFiles,
   vendor: [
     'src/bower_components/angular/angular.js',
     'src/bower_components/angular-mocks/angular-mocks.js',
@@ -18,33 +38,111 @@ var karamConfig = {
   ],
   showStack: true
 };
-gulp.task('dev',function(){
+
+
+//Buildrules for specific file sets. These help ensure that the file structure
+//remains consistent throughout the project (file not explicitly included
+//will simply be ignored by the server).
+
+gulp.task('build_bower', function (done) {
+  bower().on('end', function () {
+    fs.symlink(__dirname + '/src/bower_components', 'build/bower_components', done);
+  });
+});
+
+gulp.task('build_html', function () {
+  del('build');
+  return gulp.src(['src/app/**/*html', 'src/index.html'], {
+      base: 'src'
+    })
+    .pipe(inject(
+      gulp.src(angularFiles) .pipe(angularFileSort()),
+      {relative: true}
+     ))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build_angular', function () {
+  return gulp.src(angularFiles, {
+      base: 'src'
+    })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build_css', function () {
+  return gulp.src(['src/app/**/*sass', 'src/app/**/*scss'], {
+      base: 'src'
+    })
+    .pipe(sass())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', function () {
+  del.sync('build');
+  fs.mkdirSync('build');
+  runSequence('build_html', 'build_css', 'build_angular', 'build_bower');
+});
+
+gulp.task('watch', function () {
+  watch(angularFiles.concat(htmlFiles), {
+    base: 'src'
+  }, function (files) {
+    files.pipe(gulp.dest('build'));
+    return gulp.src(htmlFiles, {
+        base: 'src'
+      })
+      .pipe(inject(
+        gulp.src(angularFiles)
+        .pipe(angularFileSort()), {
+          relative: true
+        }))
+      .pipe(gulp.dest('build'));
+  });
+});
+
+
+//Prepared dist for server
+
+gulp.task('server', function () {
+  runSequence('build', 'watch');
+  var reload = rg.connectWatch({
+    root: './build',
+    glob: './build/**/*',
+    livereload: true
+  });
+});
+
+gulp.task('dev', function () {
   rg.connectWatch({
-    root : './src',
-    livereload : true,
-    glob : './src/**/*'
+    root: './src',
+    livereload: true,
+    glob: './src/**/*'
   });
   rg.karmaWatch(karamConfig)();
 });
 
-gulp.task('jshint',rg.jshint({
-  files : appFiles
+gulp.task('jshint', rg.jshint({
+  files: appFiles
 }));
 
-gulp.task('beautify',rg.beautify({
-  files : [appFiles[0]]
+gulp.task('beautify', rg.beautify({
+  files: [appFiles[0]]
 }));
 
-gulp.task('build',function(){
-  return runSequence('jshint','beautify');
+//Run this before comitting
+
+gulp.task('lint', function () {
+  return runSequence('jshint', 'beautify');
 });
 
 
-gulp.task('test',rg.karma(karamConfig));
+gulp.task('test', rg.karma(karamConfig));
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   console.log('***********************'.yellow);
-  console.log('  gulp dev: start a server in the  root folder and watch dev files'.yellow);
+  console.log(
+    '  gulp dev: start a server in the  root folder and watch dev files'.yellow
+  );
   console.log('  gulp test: run unit tests'.yellow);
   console.log('  gulp build: hint, lint, and minify files into ./dist '.yellow);
   console.log('***********************'.yellow);
